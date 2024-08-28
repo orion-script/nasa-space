a719be808597dde0db93ca83f53a04c466225e91
Olaniyi John <olaniyijohnayomide@gmail.com>
2024-08-28 17:00:49 +0100

    added version to the api

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const MONGO_URL =
-  "mongodb+srv://orion:WAA7rkvRn2FQ8yQA@nasa-project.erojgpe.mongodb.net/nasa?retryWrites=true&w=majority&appName=nasa-project";
+  "mongodb+srv://orion:test1234@nasa-api.s8d50.mongodb.net/?retryWrites=true&w=majority&appName=nasa-api";
 
+// Event listeners for connection
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
 });
 
 mongoose.connection.on("error", (err) => {
-  console.error(err);
+  console.error("MongoDB connection error:", err);
 });
 
 async function mongoConnect() {
