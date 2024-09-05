2bea7b9b67eedd43ef08a24a9022a52cae24524b
Olaniyi John <olaniyijohnayomide@gmail.com>
2024-09-05 12:22:53 +0100

    edit

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,18 +3,18 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100,
-  mission: "kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
-};
+// const launch = {
+//   flightNumber: 100,
+//   mission: "kepler Exploration X",
+//   rocket: "Explorer IS1",
+//   launchDate: new Date("December 27, 2030"),
+//   target: "Kepler-442 b",
+//   customers: ["ZTM", "NASA"],
+//   upcoming: true,
+//   success: true,
+// };
 
-saveLaunch(launch);
+// saveLaunch(launch);
 
 async function existsLaunchWithId(launchId) {
   return await launchesDatabase.findOne({
@@ -32,14 +32,20 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDatabase.find(
-    {},
-    {
-      _id: 0,
-      __v: 0,
-    }
-  );
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find(
+      {},
+      {
+        _id: 0,
+        __v: 0,
+      }
+    )
+    .sort({
+      flightNumber: 1,
+    })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
